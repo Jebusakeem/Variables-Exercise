c19a3e1f832b966e7c23e20fa0320e4110be3e93
Briun Greene <harmonickarma789@gmail.com>
2026-01-07 15:05:03 -0500

    added values to variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string userName = "Bman";
+            int age = 37;
+            char letter = 'a';
+            bool ofAge = true;
+            double height = 5.10;
+            decimal dollars = 5.5m;
+            
+            Console.WriteLine($"My name is {userName}, I am {age} years old");
         }
     }
 }
